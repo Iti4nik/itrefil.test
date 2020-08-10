794410fad4790c84a753d917420bbe23503929ef
Iti4nik <69465148+Iti4nik@users.noreply.github.com>
2020-08-10 16:03:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+?>
+<! DOCTYPE html>
+<html>
+	<head>
+		<title>ITRefil</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	</head>
+	<body>	
+		<header class="header">
+			<div class="container">
+				<div class="header__inner">
+					<div class="header__logo">ITRefil
+					</div>
+					<div id="nav">
+						<a class="nav__link" href="index.php">ITRefil</a>
+						<a class="nav__link" href="contact.php">Контактная информация</a>
+						<a class="nav__link" href="remont-noutbukov.php">Ремонт ноутбуков</a>
+						<a class="nav__link" href="reg.php">Регистрация</a>
+						<a class="nav__link" href="login.php">Вход</a>
+					</div>
+				</div>
+			</div>
+		</div>
+			<div class="column one divider">
+				<div class="column one">
+					<div style="text-align: center;">
+						<h3>Ремонт любой компьютерной техники.<br>Выезд мастера бесплатно!</br></h3>
+							<p><span style="font-size:16px; font-family:helvetica; color:#000000;" "text-align: center;">Качественный ремонт любой сложности. Выезд к заказчику бесплатно на дом или в офис в любой район Одессы. Всегда качественно и ответственно подхожу к ремонту компьютерной техники. Вы всегда можете провести профессиональную диагностику вашего устройства, и ремонт. Звоните!</span></p>
+				</div>
+					</div>
+			</div>
+		</header>
+	<footer id="footer-site">
+			<ul class="widget-social">
+				<li>
+					<a href=""></a>
+				</li>
+			</ul>
+			<div>
+				<span>&copy; 2020</span>
+		<a href="/">ITRefil.test</a>	
+			</div>
+			<a href="https://www.facebook.com/profile.php?id=100014945191372/"target="new" title="Facebook"> <img class="socialicon" src="Image/facebook.jpg" height="50" width="50"> </a> 
+ <a href="https://twitter.com/iamartwrk/" target="new" title="Twitter"> <img class="socialicon" src="Image/twitter.png" height="50" width="50"></a> 
+<a href="https://instagram.com/iamartwrk/" target="new" title="Instagram"> <img class="socialicon" src="Image/instagram.jpg" height="50" width="50"> </a>
+<a href="viber://chat?number=+380685778061" target="new" title="Viber"> <img class="socialicon" src="Image/viber-mobile-app-text-messaging-icon-viber-logo-png.jpg" height="50" width="50"> </a>
+	</footer>
+</body>
+</html>

16a441e546092da5f311eccfaa27a3066164dc69
Iti4nik <69465148+Iti4nik@users.noreply.github.com>
2020-08-10 18:13:23 +0300

    Add files via upload

---
+++ remont-noutbukov.php
@@ -0,0 +1,127 @@
+<?php
+session_start();
+?>
+<! DOCTYPE html>
+<html>
+	<head>
+		<title>ITRefil</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	</head>
+	<body>	
+		<header class="header">
+			<div class="container">
+				<div class="header__inner">
+					<div class="header__logo">ITRefil
+					</div>
+					<div id="nav">
+						<a class="nav__link" href="index.php">ITRefil</a>
+						<a class="nav__link" href="contact.php">Контактная информация</a>
+						<a class="nav__link" href="remont-noutbukov.php">Ремонт ноутбуков</a>
+						<a class="nav__link" href="reg.php">Регистрация</a>
+						<a class="nav__link" href="login.php">Вход</a>
+					</div>
+				</div>
+			</div>
+		</header>
+	</div>
+	<div class="Content">
+		<div class="container">
+			<div class="column one">
+				<h4><span style="font-family: helvetica;">
+								<center>Профессиональный ремонт ноутбуков</center>
+									</span></h4>
+										<p>Осуществляется профессиональный ремонт ноутбуков в Одессе всех популярных моделей представленных в магазинах марок:  Apple, Dell, Asus, Acer, Samsung, LG, Toshiba, HP. Помимо ремонта ноутбуков, можно расширить функциональные возможности ноутбука, провести сервисное обслуживание: восстановление утраченной информации, очистка системы от вредоносных программ, а также установку и настройку необходимого ПО.</p>
+										<div class="column one-second">
+											<div class="column one-second">
+												<h5>Частые причины поломки ноутбуков:</h5>
+									<ul>
+											<li>Залитие ноутбука жидкостью</li>
+											<li>Перепады напряжения в сети</li>
+											<li>Перегрев ноутбука</li>
+											<li>Выход из строя северного и южного мостов</li>
+											<li>Выход из строя мультиконтролера</li>
+											<li>Ошибка оперативной памяти при чтении</li>
+											<li>Повреждённый жесткий диск</li>
+											<li>Нет подсветки матрицы</li>
+											<li>Не загружается при включении и зависает</li>
+											<li>Не работает клавиатура</li>
+											<li>Не работают USB порты</li>
+											<li>Шумит куллер</li>
+											<li>Нет звука</li>
+											<li>Не видит сетей WI-FI</li>	
+									</ul>
+											</div>
+											<div class="column one">
+												<table style="border: 1px solid white;">
+												<tbody>
+													<tr>
+														<th>
+														<strong>Услуга</strong>
+													</th>
+													<tr>
+														<td style="border: 1px solid white;">Диагностика ноутбука</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Установка ОС (Windows, Linux, Windows Server, MacOs) включая драйвера и антивирус</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Чистка ноутбука от пыли. Замена термопасты</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замены клавиатуры ноутбука</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена матрицы ноутбука</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена разъёма зарядки (питания) ноутбука</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена корпуса ноутбука</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена внутренних шлейфов</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена и добавление оперативной памяти</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена разъема наушника (микрофона)</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена жесткого диска (HDD, SSD)</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Сброс пароля Bios</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Ремонт требующей пайки</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Устранение попадания влаги или жидкостей</td>
+													</tr>
+												</tbody>
+											</table>
+											</div>
+										</div>
+			</div>
+		</div>
+	</div>
+	<footer id="footer-site">
+			<ul class="widget-social">
+				<li>
+					<a href=""></a>
+				</li>
+			</ul>
+			<div>
+				<span>&copy; 2020</span>
+		<a href="/">ITRefil.test</a>	
+			</div>
+			<a href="https://www.facebook.com/profile.php?id=100014945191372/"target="new" title="Facebook"> <img class="socialicon" src="Image/facebook.jpg" height="50" width="50"> </a> 
+ <a href="https://twitter.com/iamartwrk/" target="new" title="Twitter"> <img class="socialicon" src="Image/twitter.png" height="50" width="50"></a> 
+<a href="https://instagram.com/iamartwrk/" target="new" title="Instagram"> <img class="socialicon" src="Image/instagram.jpg" height="50" width="50"> </a>
+<a href="viber://chat?number=+380685778061" target="new" title="Viber"> <img class="socialicon" src="Image/viber-mobile-app-text-messaging-icon-viber-logo-png.jpg" height="50" width="50"> </a>
+	</footer>
+</body>
+</html>

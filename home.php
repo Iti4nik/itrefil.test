bfeeaeeed57b3b2c8240917dc25a2ef03c81bc47
Iti4nik <69465148+Iti4nik@users.noreply.github.com>
2020-09-02 19:52:04 +0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Css/main.css">
+</head>
+<body>
+	<header>
+	</header>
+<footer>
+</footer>
+</body>
+</html>

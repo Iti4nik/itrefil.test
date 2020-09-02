f4bd3d638740a6204b7aa5fce8bdabb09bf88dbf
Iti4nik <69465148+Iti4nik@users.noreply.github.com>
2020-09-02 19:52:53 +0300

    Add files via upload

---
+++ remont komputerov.php
@@ -0,0 +1,113 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="Css/main.css">
+		<body>
+			<header class="header">
+			<div class="container">
+				<div class="header__inner">
+					<div class="header__logo">ITRefil
+					</div>
+					<div id="nav">
+						<a class="nav__link" href="index.php">ITRefil</a>
+						<a class="nav__link" href="contact.php">Контактная информация</a>
+						<a class="nav__link" href="remont-noutbukov.php">Ремонт ноутбуков</a>
+						<a class="nav__link" href="remont komputerov.php">Ремонт ПК</a>
+						<a class="nav__link" href="reg.php">Регистрация</a>
+						<a class="nav__link" href="login.php">Вход</a>
+					</div>
+				</div>
+			</div>
+			<div class="Content">
+		<div class="container">
+			<div class="column one">
+				<h4><span style="font-family: helvetica;">
+								<h5>Профессиональный ремонт ноутбуков</h5>
+									</span></h4>
+										<p>Осуществляется профессиональный ремонт ноутбуков в Одессе всех популярных моделей представленных в магазинах марок:  Apple, Dell, Asus, Acer, Samsung, LG, Toshiba, HP. Помимо ремонта ноутбуков, можно расширить функциональные возможности ноутбука, провести сервисное обслуживание: восстановление утраченной информации, очистка системы от вредоносных программ, а также установку и настройку необходимого ПО.</p>
+										<div class="column one-second">
+											<div class="column one-second">
+												<h5>Частые причины поломки ноутбуков:</h5>
+									<ul>
+											<li>Залитие ноутбука жидкостью</li>
+											<li>Перепады напряжения в сети</li>
+											<li>Перегрев ноутбука</li>
+											<li>Выход из строя северного и южного мостов</li>
+											<li>Выход из строя мультиконтролера</li>
+											<li>Ошибка оперативной памяти при чтении</li>
+											<li>Повреждённый жесткий диск</li>
+											<li>Нет подсветки матрицы</li>
+											<li>Не загружается при включении и зависает</li>
+											<li>Не работает клавиатура</li>
+											<li>Не работают USB порты</li>
+											<li>Шумит куллер</li>
+											<li>Нет звука</li>
+											<li>Не видит сетей WI-FI</li>	
+									</ul>
+											</div>
+											<div class="column one">
+												<table style="border: 1px solid white;">
+												<tbody>
+													<tr>
+														<th>
+														<strong>Услуга</strong>
+													</th>
+													<tr>
+														<td style="border: 1px solid white;">Диагностика ПК</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Установка ОС (Windows, Linux, Windows Server, MacOs) включая драйвера и антивирус</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Установка программ</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Установка антивируса</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Чистка ПК (чистка системы охлаждения от пыли, замена термопасты и термонакладок)</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Ремонт блока питания</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Сборка ПК под заказ (от стоимости комплектующих)</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Чистка ПК от пыли. Замена термопасты</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Установка / Замена корпуса ПК</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Ремонт материнской платы</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена и добавление оперативной памяти</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Замена жесткого диска (HDD, SSD)</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Сброс пароля Bios</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Ремонт требующей пайки</td>
+													</tr>
+													<tr>
+														<td style="border: 1px solid white;">Устранение попадания влаги или жидкостей</td>
+													</tr>
+												</tbody>
+											</table>
+											</div>
+										</div>
+			</div>
+		</div>	
+			</header>
+		</body>
+	</head>
+</html>
